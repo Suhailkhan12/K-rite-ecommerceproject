d670ac34dd446c3e40492f69e0cf16b948c3243d
Suhailkhan12 <suhail9717k@gmail.com>
2024-02-25 18:37:08 +0530

    add link on home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,22 @@
-import './directory-item.styles.scss';
+import "./directory-item.styles.scss";
+import { Link } from "react-router-dom";
 
-const DirectoryItem = ( {category} ) =>{
-
-    const { imageUrl, title } = category
-    return(
-        <div  className="directory-item-container">
-          <div className="background-image" style={{ backgroundImage:`url(${imageUrl})` }} />
-          <div className="body">
-            <h2>{ title }</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-    )
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <Link to="/shop">
+          <p>Shop Now</p>
+        </Link>
+      </div>
+    </div>
+  );
 };
-
-
 
 export default DirectoryItem;
